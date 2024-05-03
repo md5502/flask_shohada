94aeb6484ff53395c77462fd81dab641e41425a7
md5502 <m.baniasadi.d@gmail.com>
2024-05-03 12:22:16 +0330

    add details page

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_scss import Scss
-from datetime import datetime
+from werkzeug.utils import secure_filename
 
+import os
 app = Flask(__name__)
 Scss(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+
 db = SQLAlchemy(app)
 
 
@@ -24,7 +28,7 @@ class Martyr(db.Model):
     biography = db.Column(db.Text)  # زندگی‌نامه شهید
     photos = db.Column(db.String(1000))  # لیست مسیر عکس‌ها
     main_photo = db.Column(db.String(100))  # مسیر تصویر شاخص
-
+    
     def __repr__(self):
         return f"{self.first_name} {self.last_name}"
 
@@ -37,24 +41,71 @@ def home():
 @app.route('/insert', methods=['GET', 'POST'])
 def insert():
     if request.method == 'POST':
+        # Convert string dates to Python date objects
+        birth_date_str = request.form['birth_date']
+        martyrdom_date_str = request.form['martyrdom_date']
+        birth_date = datetime.strptime(birth_date_str, '%B %d, %Y').date() if birth_date_str else None
+        martyrdom_date = datetime.strptime(martyrdom_date_str, '%B %d, %Y').date() if martyrdom_date_str else None
+
         new_martyr = Martyr(
             first_name=request.form['first_name'],
             last_name=request.form['last_name'],
-            # ... [Add other fields as per your form] ...
+            birth_date=birth_date,
+            birth_place=request.form['birth_place'],
+            burial_place=request.form['burial_place'],
+            martyrdom_date=martyrdom_date,
+            martyrdom_place=request.form['martyrdom_place'],
+            achievements=request.form['achievements'],
+            will=request.form['will'],
+            biography=request.form['biography'],
+            photos=request.form['photos'],
+            main_photo=request.form['main_photo']
         )
+        file = request.files['main_photo_file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            new_martyr.main_photo = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
         db.session.add(new_martyr)
         db.session.commit()
         return redirect(url_for('home'))
     return render_template('insert.html')
 
+
 # Route to update a martyr's details
+from datetime import datetime
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import os
+
+# ... your existing code ...
+
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     martyr_to_update = Martyr.query.get_or_404(id)
     if request.method == 'POST':
         martyr_to_update.first_name = request.form['first_name']
         martyr_to_update.last_name = request.form['last_name']
-        # ... [Update other fields as per your form] ...
+        # Convert string dates to Python date objects
+        birth_date_str = request.form['birth_date']
+        martyrdom_date_str = request.form['martyrdom_date']
+        martyr_to_update.birth_date = datetime.strptime(birth_date_str, '%Y-%m-%d').date() if birth_date_str else None
+        martyr_to_update.martyrdom_date = datetime.strptime(martyrdom_date_str, '%Y-%m-%d').date() if martyrdom_date_str else None
+        martyr_to_update.birth_place = request.form['birth_place']
+        martyr_to_update.burial_place = request.form['burial_place']
+        martyr_to_update.martyrdom_place = request.form['martyrdom_place']
+        martyr_to_update.achievements = request.form['achievements']
+        martyr_to_update.will = request.form['will']
+        martyr_to_update.biography = request.form['biography']
+        martyr_to_update.photos = request.form['photos']
+        martyr_to_update.main_photo = request.form['main_photo']
+        # Handle file upload for main_photo
+        file = request.files.get('main_photo_file')
+        if file and file.filename != '':
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            martyr_to_update.main_photo = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         db.session.commit()
         return redirect(url_for('home'))
     return render_template('update.html', martyr=martyr_to_update)
@@ -67,7 +118,11 @@ def delete(id):
     db.session.commit()
     return redirect(url_for('home'))
 
-from datetime import date
+@app.route('/martyr/<int:id>')
+def martyr_details(id):
+    martyr = Martyr.query.get_or_404(id)
+    return render_template('martyr_details.html', martyr=martyr)
+
 
 
 if __name__ == "__main__":
